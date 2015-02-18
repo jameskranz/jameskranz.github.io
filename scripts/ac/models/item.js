02a765c43cdef563bb8cf3207ef1bd8274c15b3f
James Kranz <jkranz@fitbit.com>
2015-02-17 20:44:13 -0800

    Moving ac files, and made model for searchable items

---
+++ scripts/ac/models/item.js
@@ -0,0 +1,7 @@
+acSearch.item = DS.Model.extend({
+    name: DS.attr('string'),
+    price: DS.attr('number'),
+    location: DS.attr('string'),
+    time: DS.attr('string'),
+    months: DS.attr('string')
+});
