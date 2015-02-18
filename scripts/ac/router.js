e40001d2546d69e13bc7f2c34599ef2b42e8ca5a
James Kranz <jkranz@fitbit.com>
2015-02-17 21:04:05 -0800

    Changed template to list out items, added model to route

--- scripts/ac/router.js
+++ scripts/ac/router.js
@@ -1,3 +1,8 @@
+acSearch.acSearchRoute = Ember.Route.extend({
+    model: function () {
+        return this.store.find('item');
+    }
+});
 
 acSearch.Router.map(function() {
     this.resource('ac-search', { path: '/' });
