6395156972bb4ea13e0af50c8b5c7aec9802c9aa
James Kranz <jkranz@fitbit.com>
2015-02-18 22:10:02 -0800

    Controller fix.

--- scripts/ac/controllers/items_controller.js
+++ scripts/ac/controllers/items_controller.js
@@ -1,4 +1,4 @@
-acSearch.ItemsController = Ember.ArrayController.extend({
+acSearch.AcSearchController = Ember.ArrayController.extend({
     actions: {
         doSearch: function () {
             console.log('searching', arguments);
--- scripts/ac/models/item.js
+++ scripts/ac/models/item.js
@@ -1,24 +1,26 @@
 acSearch.Item = DS.Model.extend({
-    name: DS.attr('string')
-    //name: DS.attr('string'),
-    //price: DS.attr('number'),
-    //location: DS.attr('string'),
-    //time: DS.attr('string'),
-    //months: DS.attr('string')
+    name: DS.attr('string'),
+    price: DS.attr('number'),
+    location: DS.attr('string'),
+    time: DS.attr('string'),
+    months: DS.attr('string')
 });
 
 acSearch.Item.FIXTURES = [
     {
         id: 1,
-        name: 'Search for items by name!'
-        //,
-        //price: 0,
-        //location: 'xxx',
-        //time: 'xxx',
-        //months: 'xxx'
+        name: 'Search for items by name!',
+        price: 0,
+        location: 'xxx',
+        time: 'xxx',
+        months: 'xxx'
     },
     {
         id: 2,
-        name: 'test'
+        name: 'test',
+        price: 0,
+        location: 'xxx',
+        time: 'xxx',
+        months: 'xxx'
     }
 ];
