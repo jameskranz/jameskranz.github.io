866fc1397224a976e734e89424ed9ee7c94d06a5
James Kranz <jkranz@fitbit.com>
2015-02-18 19:08:36 -0800

    Route is working! Now trying to fix missing model.

--- scripts/ac/models/item.js
+++ scripts/ac/models/item.js
@@ -1,18 +1,20 @@
 acSearch.item = DS.Model.extend({
-    name: DS.attr('string'),
-    price: DS.attr('number'),
-    location: DS.attr('string'),
-    time: DS.attr('string'),
-    months: DS.attr('string')
+    name: DS.attr('string')
+    //name: DS.attr('string'),
+    //price: DS.attr('number'),
+    //location: DS.attr('string'),
+    //time: DS.attr('string'),
+    //months: DS.attr('string')
 });
 
 acSearch.item.FIXTURES = [
     {
         id: 1,
-        name: 'Search for items by name!',
-        price: 0,
-        location: 'xxx',
-        time: 'xxx',
-        months: 'xxx'
+        name: 'Search for items by name!'
+        //,
+        //price: 0,
+        //location: 'xxx',
+        //time: 'xxx',
+        //months: 'xxx'
     }
 ];
