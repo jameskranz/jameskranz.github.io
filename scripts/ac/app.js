238d8286c027b011b2118cba10eff238975372f5
James Kranz <jkranz@fitbit.com>
2015-03-03 23:30:45 -0800

    Adapter change to see if that helps fix my pushPayload issues.

--- scripts/ac/app.js
+++ scripts/ac/app.js
@@ -2,5 +2,6 @@ window.acSearch = Ember.Application.create({
     LOG_TRANSITIONS: true
 });
 
-acSearch.ApplicationAdapter = DS.FixtureAdapter.extend();
+//acSearch.ApplicationAdapter = DS.FixtureAdapter.extend();
+acSearch.ApplicationAdapter = DS.RESTAdapter.extend();
 acSearch.ApplicationSerializer = DS.JSONSerializer;
