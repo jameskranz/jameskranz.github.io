d600a218378305316c0876130409061fd003f420
James Kranz <jkranz@fitbit.com>
2015-03-03 23:42:10 -0800

    Experimenting with extending fixture adapter.

--- scripts/ac/app.js
+++ scripts/ac/app.js
@@ -2,8 +2,12 @@ window.acSearch = Ember.Application.create({
     LOG_TRANSITIONS: true
 });
 
-//acSearch.ApplicationAdapter = DS.FixtureAdapter.extend();
-acSearch.ApplicationAdapter = DS.LSAdapter.extend({
-    namespace: 'acsearch'
+acSearch.ApplicationAdapter = DS.FixtureAdapter.extend({
+    pushPayload: function () {
+        console.log('derp');
+    }
 });
+//acSearch.ApplicationAdapter = DS.LSAdapter.extend({
+    //namespace: 'acsearch'
+//});
 acSearch.ApplicationSerializer = DS.JSONSerializer;
