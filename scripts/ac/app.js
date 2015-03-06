f66fc4bb9e48b2ad4d659582fc215fa2bfca9b64
James Kranz <jkranz@fitbit.com>
2015-03-05 18:10:01 -0800

    Moving extenstion from adapter to serializer

--- scripts/ac/app.js
+++ scripts/ac/app.js
@@ -2,12 +2,12 @@ window.acSearch = Ember.Application.create({
     LOG_TRANSITIONS: true
 });
 
-acSearch.ApplicationAdapter = DS.FixtureAdapter.extend({
+acSearch.ApplicationAdapter = DS.FixtureAdapter;
+//acSearch.ApplicationAdapter = DS.LSAdapter.extend({
+    //namespace: 'acsearch'
+//});
+acSearch.ApplicationSerializer = DS.JSONSerializer.extend({
     pushPayload: function () {
         console.log('derp');
     }
 });
-//acSearch.ApplicationAdapter = DS.LSAdapter.extend({
-    //namespace: 'acsearch'
-//});
-acSearch.ApplicationSerializer = DS.JSONSerializer;
