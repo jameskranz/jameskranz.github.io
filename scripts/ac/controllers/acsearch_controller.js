cb92f0b096c6ffd1aa7bc7b97c70fc96cf458a0b
James Kranz <jkranz@fitbit.com>
2015-03-09 11:17:56 -0700

    Fuzzy search, space delimited

--- scripts/ac/controllers/acsearch_controller.js
+++ scripts/ac/controllers/acsearch_controller.js
@@ -6,11 +6,22 @@ acSearch.AcSearchController = Ember.ArrayController.extend({
     },
 
     searchResults: function () {
-        var searchTerm = this.get('searchTerm');
-        var regexp = new RegExp(searchTerm, 'i');
+        var searchTermRaw = this.get('searchTerm');
+        var searchTerms = searchTermRaw.split(' ');
+        var regexps = [], i, l;
+
+        for (i = 0, l = searchTerms.length; i < l; i++) {
+            regexps.push(new RegExp(searchTerms[i], 'i'));
+        }
 
         var filteredResults = this.filter(function (item) {
+            var result = true;
             var name = item.get('name');
+
+            for (i = 0, l = regexps.length; result && i < l; i++) {
+                result = regexps[i].test(name);
+            }
+
             return regexp.test(name);
         });
 
