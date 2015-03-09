1868b00b21246e28beb663209e754e2d402704f4
James Kranz <jkranz@fitbit.com>
2015-03-08 17:49:01 -0700

    Adding raw fish data.

---
+++ scripts/ac/fish.data.js
@@ -0,0 +1,290 @@
+[
+{
+    "name": "Angelfish",
+    "price": 3000
+},
+{
+    "name": "Arapaima",
+    "price": 10000
+},
+{
+    "name": "Arowana",
+    "price": 10000
+},
+{
+    "name": "Barbel Steed",
+    "price": 200
+},
+{
+"name": "Barred Knifejaw",
+"price": 5000
+},
+{
+"name": "Bitterling",
+"price": 900
+},
+{
+"name": "Black Bass",
+"price": 300
+},
+{
+"name": "Blowfish",
+"price": 125
+},
+{
+"name": "Blue Marlin",
+"price": 10000
+},
+{
+"name": "Bluegill",
+"price": 120
+},
+{
+"name": "Butterfly Fish",
+"price": 1000
+},
+{
+"name": "Carp",
+"price": 300
+},
+{
+"name": "Catfish",
+"price": 800
+},
+{
+"name": "Char",
+"price": 3800
+},
+{
+"name": "Cherry Salmon",
+"price": 1000
+},
+{
+"name": "Clownfish",
+"price": 650
+},
+{
+"name": "Coelacanth",
+"price": 15000
+},
+{
+"name": "Crawfish",
+"price": 200
+},
+{
+"name": "Crucian Carp",
+"price": 120
+},
+{
+"name": "Dab",
+"price": 300
+},
+{
+"name": "Dace",
+"price": 200
+},
+{
+"name": "Dorado",
+"price": 15000
+},
+{
+"name": "Eel",
+"price": 2000
+},
+{
+"name": "Football Fish",
+"price": 2500
+},
+{
+"name": "Freshwater Goby",
+"price": 300
+},
+{
+"name": "Frog",
+"price": 120
+},
+{
+"name": "Gar",
+"price": 6000
+},
+{
+"name": "Giant Snakehead",
+"price": 5500
+},
+{
+"name": "Giant Trevally",
+"price": 4500
+},
+{
+"name": "Goldfish",
+"price": 1300
+},
+{
+"name": "Guppy",
+"price": 1300
+},
+{
+"name": "Hammerhead Shark",
+"price": 8000
+},
+{
+"name": "Horse Mackerel",
+"price": 150
+},
+{
+"name": "Killifish",
+"price": 300
+},
+{
+"name": "King Salmon",
+"price": 1800
+},
+{
+"name": "Koi",
+"price": 4000
+},
+{
+"name": "Loach",
+"price": 300
+},
+{
+"name": "Mitten Crab",
+"price": 2000
+},
+{
+"name": "Moray Eel",
+"price": 2000
+},
+{
+"name": "Napoleonfish",
+"price": 10000
+},
+{
+"name": "Neon Tetra",
+"price": 500
+},
+{
+"name": "Nibble Fish",
+"price": 1500
+},
+{
+"name": "Oarfish",
+"price": 9000
+},
+{
+"name": "Ocean Sunfish",
+"price": 4000
+},
+{
+"name": "Olive Flounder",
+"price": 800
+},
+{
+"name": "Pale Chub",
+"price": 200
+},
+{
+"name": "Pike",
+"price": 1800
+},
+{
+"name": "Piranha",
+"price": 2500
+},
+{
+"name": "Pond Smelt",
+"price": 300
+},
+{
+"name": "Popeyed Goldfish",
+"price": 1300
+},
+{
+"name": "Puffer Fish",
+"price": 240
+},
+{
+"name": "Rainbow Trout",
+"price": 800
+},
+{
+"name": "Ray",
+"price": 3000
+},
+{
+"name": "Red Snapper",
+"price": 3000
+},
+{
+"name": "Ribbon Eel",
+"price": 600
+},
+{
+"name": "Saddled Bichir",
+"price": 4000
+},
+{
+"name": "Salmon",
+"price": 700
+},
+{
+"name": "Saw Shark",
+"price": 12000
+},
+{
+"name": "Sea Bass",
+"price": 200
+},
+{
+"name": "Sea Butterfly",
+"price": 1000
+},
+{
+"name": "Seahorse",
+"price": 1100
+},
+{
+"name": "Shark",
+"price": 15000
+},
+{
+"name": "Soft Shelled Turtle",
+"price": 3750
+},
+{
+"name": "Squid",
+"price": 400
+},
+{
+"name": "Stringfish",
+"price": 15000
+},
+{
+"name": "Surgeonfish",
+"price": 1000
+},
+{
+"name": "Sweetfish",
+"price": 900
+},
+{
+"name": "Tadpole",
+"price": 100
+},
+{
+"name": "Tuna",
+"price": 7000
+},
+{
+"name": "Whale Shark",
+"price": 13000
+},
+{
+"name": "Yellow Perch",
+"price": 240
+},
+{
+"name": "Zebra Turkeyfish",
+"price": 400
+}
+]
